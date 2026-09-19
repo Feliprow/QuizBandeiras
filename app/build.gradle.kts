bb951352b294e50508685b0a6a1d46f71e01fc48
Felipe Neres Vieira <feliprow@MacBook-Air-de-Feliprow.local>
2026-09-19 19:45:23 -0300

    Remove dependência activity-ktx sem uso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,41 @@
-plugins {
-    alias(libs.plugins.android.application)
-}
-
-android {
-    namespace = "com.local.feliprow.quizbandeiras"
-    compileSdk {
-        version = release(37)
+    plugins {
+        alias(libs.plugins.android.application)
     }
 
-    defaultConfig {
-        applicationId = "com.local.feliprow.quizbandeiras"
-        minSdk = 24
-        targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+    android {
+        namespace = "com.local.feliprow.quizbandeiras"
+        compileSdk {
+            version = release(37)
+        }
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+        defaultConfig {
+            applicationId = "com.local.feliprow.quizbandeiras"
+            minSdk = 24
+            targetSdk = 37
+            versionCode = 1
+            versionName = "1.0"
 
-    buildTypes {
-        release {
-            optimization {
-                enable = false
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+        buildTypes {
+            release {
+                optimization {
+                    enable = false
+                }
             }
         }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
 
-dependencies {
-    implementation(libs.activity.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.constraintlayout)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(libs.ext.junit)
-}
+    dependencies {
+        implementation(libs.appcompat)
+        implementation(libs.constraintlayout)
+        implementation(libs.material)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.espresso.core)
+        androidTestImplementation(libs.ext.junit)
+    }
